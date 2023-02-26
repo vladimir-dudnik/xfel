45ab3227bb20aadd5c0771ba71a774173ac02744
Vladimir Dudnik <vladimir.dudnik@gmail.com>
2023-02-26 14:52:00 +0300

    tmp

---
+++ spitest.c
@@ -0,0 +1,24 @@
+#include <spitest.h>
+
+struct spi_pdata_t {
+	uint32_t swapbuf;
+	uint32_t swaplen;
+	uint32_t cmdlen;
+};
+
+
+static int spi_helper_init(struct xfel_ctx_t* ctx, struct spi_pdata_t* pdat)
+{
+	return fel_spi_init(ctx, &pdat->swapbuf, &pdat->swaplen, &pdat->cmdlen);
+}
+
+
+int spi_test(struct xfel_ctx_t* ctx)
+{
+	struct spi_pdata_t pdat;
+
+    if(spi_helper_init(ctx, &pdat))
+        return 1;
+
+    return 0;
+}
---
+++ spitest.h
@@ -0,0 +1,16 @@
+#ifndef __SPITEST_H__
+#define __SPITEST_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <fel.h>
+
+int spi_test(struct xfel_ctx_t * ctx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SPITEST_H__ */
